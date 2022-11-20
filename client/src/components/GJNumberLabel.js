479d4180a74488216bc1c30962bce45055564390
militabu <buzaitemilita@gmail.com>
2022-11-19 22:11:14 -1000

    installed proxy for cors erros

--- client/src/components/GJNumberLabel.js
+++ client/src/components/GJNumberLabel.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Button } from './Button'
 import GJNumbersView from './GJNumbersView'
 
-export default function TradingPairs() {
+export default function GJNumberLabel() {
 
     const [buttonPairs, setButtonPairs] = useState([])
     const [clickedButtonPair, setClickedButtonPair] = useState('')
